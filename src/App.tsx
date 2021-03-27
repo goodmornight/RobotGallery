3f3a1ca99234c961847fda499fe43f3c67930f9c
GoodNight <goodnightlilanhui@gmail.com>
2021-03-27 19:05:02 +0800

    feat: shoppingcart

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,62 @@
 import React from 'react';
-// import logo from './logo.svg';
+import logo from './assets/images/logo.svg';
 // import './App.css';
 import styles from './App.module.css';
-import robots from './mockdata/robots.json';
+// import robots from './mockdata/robots.json';
 import Robot from './components/Robot';
+import ShoppingCart from './components/ShoppingCart';
 
-function App() {
-  return (
-    <div className={styles.app}>
-      <div className={styles.robotList}>
-        {robots.map( r => <Robot id={r.id} email={r.email} name={r.name}/> )}
+interface Props {
+
+}
+
+interface State {
+  robotGallery: any[]
+  count: number
+}
+
+class App extends React.Component<Props, State> {
+  constructor(props) {
+    super(props)
+    this.state = {
+      robotGallery: [],
+      count: 0,
+    };
+  }
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then(res => res.json())
+    .then(data => this.setState({robotGallery: data}))
+  }
+  render() {
+    return (
+      <div className={styles.app}>
+        <div className={styles.appHeader}>
+          <img src={logo} className={styles.appLogo} alt="logo"/>
+          <h1>罗伯特机器人炫酷吊炸天online购物平台的名字要长</h1>
+          <button onClick={()=>{
+            this.setState((preState, preProps)=>{
+              return {count: preState.count+1}
+            }, ()=>{
+              console.log('count', this.state.count)
+            });
+            this.setState((preState, preProps)=>{
+              return {count: preState.count+1}
+            }, ()=>{
+              console.log('count', this.state.count)
+            });
+          }}>
+            count: {this.state.count}
+          </button>
+        </div>
+        <ShoppingCart />
+        <div className={styles.robotList}>
+          {this.state.robotGallery.map( r => <Robot id={r.id} email={r.email} name={r.name}/> )}
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
+  
 }
 
 export default App;
