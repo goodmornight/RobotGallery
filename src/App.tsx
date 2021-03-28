dd495bbffc0da6f5ed24b398bf42767567ed21bb
GoodNight <goodnightlilanhui@gmail.com>
2021-03-28 23:48:10 +0800

    feat: hook && HOC

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './assets/images/logo.svg';
 // import './App.css';
 import styles from './App.module.css';
 // import robots from './mockdata/robots.json';
 import Robot from './components/Robot';
+import RobotDiscount from './components/RobotDiscount';
 import ShoppingCart from './components/ShoppingCart';
 
 interface Props {
@@ -15,47 +16,59 @@ interface State {
   count: number
 }
 
-class App extends React.Component<Props, State> {
-  constructor(props) {
-    super(props)
-    this.state = {
-      robotGallery: [],
-      count: 0,
-    };
-  }
-  componentDidMount() {
-    fetch("https://jsonplaceholder.typicode.com/users")
-    .then(res => res.json())
-    .then(data => this.setState({robotGallery: data}))
-  }
-  render() {
-    return (
-      <div className={styles.app}>
-        <div className={styles.appHeader}>
-          <img src={logo} className={styles.appLogo} alt="logo"/>
-          <h1>罗伯特机器人炫酷吊炸天online购物平台的名字要长</h1>
-          <button onClick={()=>{
-            this.setState((preState, preProps)=>{
-              return {count: preState.count+1}
-            }, ()=>{
-              console.log('count', this.state.count)
-            });
-            this.setState((preState, preProps)=>{
-              return {count: preState.count+1}
-            }, ()=>{
-              console.log('count', this.state.count)
-            });
-          }}>
-            count: {this.state.count}
-          </button>
-        </div>
-        <ShoppingCart />
-        <div className={styles.robotList}>
-          {this.state.robotGallery.map( r => <Robot id={r.id} email={r.email} name={r.name}/> )}
-        </div>
+const App: React.FC = (props) => {
+  const [count, setCount] = useState<number>(0);
+  const [robotGallery, setRobotGallery] = useState<any>([]);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string>();
+
+  useEffect(()=>{
+    document.title = `点击${count}次`
+  },[count]);
+
+  useEffect(()=>{
+    setLoading(true);
+    const fetchData = async () => {
+      try {
+        const response = await fetch("https://jsonplaceholder.typicode.com/users");
+        const data = await response.json();
+        setRobotGallery(data);
+      } catch(e) {
+        setError(e.message);
+      }
+        setLoading(false);
+    }
+    fetchData();
+  },[]);
+
+  return (
+    <div className={styles.app}>
+      <div className={styles.appHeader}>
+        <img src={logo} className={styles.appLogo} alt="logo"/>
+        <h1>罗伯特机器人炫酷吊炸天online购物平台的名字要长</h1>
       </div>
-    );
-  }
+      <button onClick={()=>{
+        setCount(count+1)
+      }}>
+        count: {count}
+      </button>
+      <ShoppingCart />
+      { error && <div>网站出错：{error}</div> }
+      {!loading ? (
+        <div className={styles.robotList}>
+          {robotGallery.map( (r, index) => 
+            index % 2 === 0 ? (
+              <RobotDiscount id={r.id} email={r.email} name={r.name}/>
+            ):(
+              <Robot id={r.id} email={r.email} name={r.name}/>
+            ) )}
+        </div>
+      ) : (
+        <h1>正在加载...</h1>
+      )
+      }
+    </div>
+  );
   
 }
 
